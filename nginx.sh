45e852a82ade64bc6cf601232d6cde0f23831135
Dick Tang <dick.tang@ymail.com>
2016-11-10 23:19:48 +0800

    revamp nginx.conf; install nginx-extra

--- nginx.sh
+++ nginx.sh
@@ -2,31 +2,61 @@
 cat - > /nginx.conf <<EOF
 events {
 }
+
 user root;
+
 http {
-    error_log /dev/stdout info;
+    error_log /dev/stderr;
     access_log /dev/stdout;
+    default_type  application/json;
     server {
-        listen        $1;
-        ssl on;
+        listen $1 default_server ssl;
 
         ssl_certificate      $2;
         ssl_certificate_key  $3;
         ssl_client_certificate $4;
         ssl_verify_client off;
 
+        keepalive_timeout   70;
+
         location / {
+            proxy_pass_header Server;
+            proxy_http_version 1.1;
             proxy_pass http://unix:$5:;
         }
-        location /v1.22/version {
+        location /version {
+            proxy_pass_header Server;
             proxy_set_header Accept-Encoding "";
+            proxy_http_version 1.1;
+            proxy_pass http://unix:/var/run/docker.sock:;
+            sub_filter_once off;
+            sub_filter_types application/json;
+            sub_filter '1.11.2' '1.11.2-cs5';
+            sub_filter 'b9f10c9' 'd364ea1';
+        }
+        location /v1.22/version {
+            proxy_pass_header Server;
+            proxy_set_header Accept-Encoding "";
+            proxy_http_version 1.1;
+            proxy_pass http://unix:/var/run/docker.sock:;
+            sub_filter_once off;
+            sub_filter_types application/json;
+            sub_filter '1.11.2' '1.11.2-cs5';
+            sub_filter 'b9f10c9' 'd364ea1';
+        }
+        location /info {
+            proxy_pass_header Server;
+            proxy_set_header Accept-Encoding "";
+            proxy_http_version 1.1;
             proxy_pass http://unix:/var/run/docker.sock:;
             sub_filter_once off;
             sub_filter_types application/json;
             sub_filter '1.11.2' '1.11.2-cs5';
         }
         location /v1.22/info {
+            proxy_pass_header Server;
             proxy_set_header Accept-Encoding "";
+            proxy_http_version 1.1;
             proxy_pass http://unix:/var/run/docker.sock:;
             sub_filter_once off;
             sub_filter_types application/json;
